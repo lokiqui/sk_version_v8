18df7637d8cd6b1c4798a446195c58611a0d733a
lokiqui <luisiozzi@gmail.com>
2023-10-26 23:08:24 -0300

    Ya ni se

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'authentication.starter.template',
+  appName: 'authentication-starter-template',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
